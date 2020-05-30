15d318338e83ab0ea2927f9a00abdcffd7c8f9c4
Lukasz Macon <macon.lukasz@wp.pl>
2020-05-30 23:53:20 +0200

    create templates view

--- src/View/View.php
+++ src/View/View.php
@@ -7,7 +7,6 @@ class View
 
   public function render(): void // method to show functions in class
   {
-
 ?>
     <html lang="pl">
 
@@ -20,38 +19,15 @@ class View
       <div class="wrapper">
 
         <div>
-          Lista wszystkich osób: <br>
-          <?php
-
-          ?>
-        </div>
-        <br>
-
-        <div>
-          Lista wszystkich firm: <br>
-          <?php
-
-          ?>
-        </div>
-        <br>
-
-        <div>
-          Lista wszystkich zamówień: <br>
-
-          <?php
-
-
-          ?>
-
-        </div>
-        <br>
-        <div>
-          Lista wszystkich faktur: <br>
-
-          <?php
-
-
-          ?>
+          <ul style="line-height:30px;">
+            <li><a href="src/View/templates/new_purchase.php">New purchase</a></li>
+            <li><a href="src/View/templates/purchase_list.php">Purchase list</a></li>
+            <li><a href="src/View/templates/item_list.php">Item list</a></li>
+            <li><a href="src/View/templates/invoice_list.php">Invoice list</a></li>
+            <li><a href="src/View/templates/company_list.php">Company list</a></li>
+            <li><a href="src/View/templates/worker_list.php">Worker list</a></li>
+            <li><a href="src/View/templates/customer_list.php">Customer list</a></li>
+          </ul>
 
         </div>
 
