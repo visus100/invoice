3ade7927d36d3fafa08936a6fe7d654f00ea60d8
Lukasz Macon <macon.lukasz@wp.pl>
2020-05-31 21:01:21 +0200

    purchase query selection create

--- src/Model/Purchase.php
+++ src/Model/Purchase.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types=1);
-
+require_once "../../Utils/debug.php";
 class Purchase extends AbstractModel
 {
     private $id;
@@ -31,10 +31,59 @@ class Purchase extends AbstractModel
 
         self::startSQLConnection($config);
 
+
+
         try {
-            $query = "SELECT * FROM Purchase";
+
+
+            //dane informacyjne
+            $query = "SELECT purchase.id, purchase.date_of_order, purchase.id_worker, purchase.id_customer, worker.name, worker.surname, worker.phone, worker.work_position, invoice.invoice_number, invoice.id_company, invoice.id_purchase, invoice.id_worker FROM purchase 
+            INNER JOIN customer ON Purchase.id_customer = customer.id 
+            INNER JOIN worker ON Purchase.id_worker = worker.id
+            LEFT JOIN invoice ON Purchase.id = invoice.id_purchase";
             $result = self::$conn->query($query);
-            $db_list = $result->fetchAll(PDO::FETCH_ASSOC); // save all records
+            $purchase_data_list = $result->fetchAll(PDO::FETCH_ASSOC); // save all records
+
+
+
+            //lista towarów w zamówieniu
+            $query = "SELECT purchase_item.id_purchase  , purchase_item.id_item, item.name , item.price_per_unit FROM purchase
+            INNER JOIN purchase_item ON purchase.id = purchase_item.id_purchase
+            INNER JOIN item ON purchase_item.id_item = item.id";
+            $result = self::$conn->query($query);
+            $pruchase_item_list = $result->fetchAll(PDO::FETCH_ASSOC); // save all records
+
+            //lista towarów na fakturze
+            $query = "SELECT invoice.id_purchase, invoice.invoice_number, invoice_item.id_invoice, invoice_item.id_item, item.name, item.price_per_unit FROM invoice 
+            INNER JOIN purchase ON invoice.id_purchase = purchase.id 
+            INNER JOIN invoice_item ON invoice.id = invoice_item.id_invoice
+            INNER JOIN item ON invoice_item.id_item = item.id";
+            $result = self::$conn->query($query);
+            $invoice_item_list = $result->fetchAll(PDO::FETCH_ASSOC); // save all records
+
+            //  dump($invoice_item_list);
+
+            $db_list = $purchase_data_list;
+
+            for ($i = 0; $i < count($db_list); $i++) {
+                $db_list[$i]["pruchase_item_list"] = [];
+                $db_list[$i]["invoice_item_list"] = [];
+
+                for ($n = 0; $n < count($pruchase_item_list); $n++) {
+                    if ($pruchase_item_list[$n]["id_purchase"] === $db_list[$i]["id"]) {
+                        $db_list[$i]["pruchase_item_list"][] = $pruchase_item_list[$n];
+                    }
+                }
+
+                for ($n = 0; $n < count($invoice_item_list); $n++) {
+                    if ($invoice_item_list[$n]["id_purchase"] === $db_list[$i]["id"]) {
+                        $db_list[$i]["invoice_item_list"][] = $invoice_item_list[$n];
+                    }
+                }
+            }
+
+
+            dump($db_list);
 
             return $db_list;
         } catch (Throwable $e) {
